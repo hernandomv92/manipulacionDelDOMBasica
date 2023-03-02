789232c7b4a6ae0eabd6aa4f6066e88b4060c371
hernandomv <hernandomv@gmai.com>
2023-03-02 16:30:39 -0500

    Primer commit .gitignore

---
+++ script.js
@@ -0,0 +1,14 @@
+const form = document.querySelector('#form');
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btn = document.querySelector('#btnCalcular');
+const pResult = document.querySelector('#result');
+
+form.addEventListener('submit', sumarInputValues);
+
+function sumarInputValues(event) {
+    event.preventDefault();
+    const sumaInputs = input1.value + input2.value;
+    pResult.innerText = "Resultado: " + sumaInputs;
+}
+
